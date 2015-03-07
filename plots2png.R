b28cfc7fdfa94322d64f606ecb0acd49154bd309
Daniel <daniel.villegas.p@gmail.com>
2015-03-07 17:47:26 -0500

    Add correct heigth and transparent bg

--- plots2png.R
+++ plots2png.R
@@ -1,4 +1,6 @@
 ## Save plots to png
+
+## Images where saved to the directory called "img"
 source('./read.power.R')
 source('./plot1.R')
 source('./plot2.R')
@@ -15,16 +17,16 @@ plots2png <- function(srcfile='./RData/household_power_consumption.txt',
     if(!file.exists(destfile)){
         dir.create(destfile)
     }
-    png(file=paste(destfile, "/plot1.png", collapse="", sep=""), width=480, heigth=480, units="px")
+    png(file=paste(destfile, "/plot1.png", collapse="", sep=""), width=480, height=480, units="px", bg="transparent")
     plot1(powerData)
     dev.off()
-    png(file=paste(destfile, "/plot2.png", collapse="", sep=""), width=480, heigth=480, units="px")
+    png(file=paste(destfile, "/plot2.png", collapse="", sep=""), width=480, height=480, units="px", bg="transparent")
     plot2(powerData)
     dev.off()
-    png(file=paste(destfile, "/plot3.png", collapse="", sep=""), width=480, heigth=480, units="px")
+    png(file=paste(destfile, "/plot3.png", collapse="", sep=""), width=480, height=480, units="px", bg="transparent")
     plot3(powerData)
     dev.off()
-    png(file=paste(destfile, "/plot4.png", collapse="", sep=""), width=480, heigth=480, units="px")
+    png(file=paste(destfile, "/plot4.png", collapse="", sep=""), width=480, height=480, units="px", bg="transparent")
     plot4(powerData)
     dev.off()
 }
